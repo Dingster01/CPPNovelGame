2c42df06e4ab0dacbe8ac2c555e0f3d97b213733
LAPTOP-9DQS12E1\Henry Ding <henry1998628@gmail.com>
2019-02-08 15:07:34 -0800

    Initial Commit. Created folders.

---
+++ CPPNovelGame/Intermediate/Build/Win64/CPPNovelGameEditor/Development/Engine/SharedPCH.Engine.cpp
@@ -0,0 +1,1 @@
+#include "E:/GitHub Projects Folder/CPPNovelGame/CPPNovelGame/Intermediate/Build/Win64/CPPNovelGameEditor/Development/Engine/SharedPCH.Engine.h"
---
+++ CPPNovelGame/Intermediate/Build/Win64/UE4Editor/Inc/CPPNovelGame/CPPNovelGameGameModeBase.generated.h
@@ -0,0 +1,90 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef CPPNOVELGAME_CPPNovelGameGameModeBase_generated_h
+#error "CPPNovelGameGameModeBase.generated.h already included, missing '#pragma once' in CPPNovelGameGameModeBase.h"
+#endif
+#define CPPNOVELGAME_CPPNovelGameGameModeBase_generated_h
+
+#define CPPNovelGame_Source_CPPNovelGame_CPPNovelGameGameModeBase_h_15_RPC_WRAPPERS
+#define CPPNovelGame_Source_CPPNovelGame_CPPNovelGameGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define CPPNovelGame_Source_CPPNovelGame_CPPNovelGameGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesACPPNovelGameGameModeBase(); \
+	friend struct Z_Construct_UClass_ACPPNovelGameGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(ACPPNovelGameGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient), CASTCLASS_None, TEXT("/Script/CPPNovelGame"), NO_API) \
+	DECLARE_SERIALIZER(ACPPNovelGameGameModeBase)
+
+
+#define CPPNovelGame_Source_CPPNovelGame_CPPNovelGameGameModeBase_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesACPPNovelGameGameModeBase(); \
+	friend struct Z_Construct_UClass_ACPPNovelGameGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(ACPPNovelGameGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient), CASTCLASS_None, TEXT("/Script/CPPNovelGame"), NO_API) \
+	DECLARE_SERIALIZER(ACPPNovelGameGameModeBase)
+
+
+#define CPPNovelGame_Source_CPPNovelGame_CPPNovelGameGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ACPPNovelGameGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ACPPNovelGameGameModeBase) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ACPPNovelGameGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ACPPNovelGameGameModeBase); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ACPPNovelGameGameModeBase(ACPPNovelGameGameModeBase&&); \
+	NO_API ACPPNovelGameGameModeBase(const ACPPNovelGameGameModeBase&); \
+public:
+
+
+#define CPPNovelGame_Source_CPPNovelGame_CPPNovelGameGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ACPPNovelGameGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ACPPNovelGameGameModeBase(ACPPNovelGameGameModeBase&&); \
+	NO_API ACPPNovelGameGameModeBase(const ACPPNovelGameGameModeBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ACPPNovelGameGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ACPPNovelGameGameModeBase); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ACPPNovelGameGameModeBase)
+
+
+#define CPPNovelGame_Source_CPPNovelGame_CPPNovelGameGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET
+#define CPPNovelGame_Source_CPPNovelGame_CPPNovelGameGameModeBase_h_12_PROLOG
+#define CPPNovelGame_Source_CPPNovelGame_CPPNovelGameGameModeBase_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	CPPNovelGame_Source_CPPNovelGame_CPPNovelGameGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	CPPNovelGame_Source_CPPNovelGame_CPPNovelGameGameModeBase_h_15_RPC_WRAPPERS \
+	CPPNovelGame_Source_CPPNovelGame_CPPNovelGameGameModeBase_h_15_INCLASS \
+	CPPNovelGame_Source_CPPNovelGame_CPPNovelGameGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define CPPNovelGame_Source_CPPNovelGame_CPPNovelGameGameModeBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	CPPNovelGame_Source_CPPNovelGame_CPPNovelGameGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	CPPNovelGame_Source_CPPNovelGame_CPPNovelGameGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	CPPNovelGame_Source_CPPNovelGame_CPPNovelGameGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	CPPNovelGame_Source_CPPNovelGame_CPPNovelGameGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID CPPNovelGame_Source_CPPNovelGame_CPPNovelGameGameModeBase_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
